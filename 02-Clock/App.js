696f6e68b3df9ee009d09f96606527b3a723b418
Fatmanur Bulut <bfatmanur38@gmail.com>
2023-02-16 08:15:25 +0300

    Upload 02-Clock Folder

---
+++ 02-Clock/App.js
@@ -0,0 +1,13 @@
+const analog = document.querySelector("#analog");
+const digital = document.querySelector("#digital");
+const container = document.querySelector(".container");
+
+analog.addEventListener('click', analogClock);
+digital.addEventListener('click', digitalClock);
+
+function analogClock(){
+}
+
+function digitalClock(){
+
+}
