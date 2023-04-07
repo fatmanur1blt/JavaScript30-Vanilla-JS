7ecc76c4b022fc759ae90d072f4d1309857cb35b
Fatmanur Bulut <bfatmanur38@gmail.com>
2023-04-07 22:24:47 +0300

    update file.

--- 17-Sort-Without-Numbers/app.js
+++ 17-Sort-Without-Numbers/app.js
@@ -1,8 +1,7 @@
 const singers = ['1 Jesssi J', '2 Ariana Grande', '3 Selena Gomez' ,'4 Lady Gaga', '5 Eminem', '6 Sam Smith','7 Frank Sinatra','8 Alan Walker', '9 Rita Ora', '10 Charlie Puth'];
 
 function strip(singerName){
-    return singerName.replace(/(1 |2 |3 |4 |5 |6 |7 |8 |9 |10 )/i, '').trim();
-    // used for case sensitivity (/i).
+    return singerName.replace(/(1 |2 |3 |4 |5 |6 |7 |8 |9 |10 )/, '').trim();
 }
 
 const sortSingers = singers.sort((a,b) => strip(a) > strip(b) ? 1 : -1);
