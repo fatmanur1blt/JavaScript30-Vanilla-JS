7bf2f2f3eaac358ef45e61c98ca1bc0d55894ff3
Fatmanur Bulut <bfatmanur38@gmail.com>
2023-04-19 00:57:40 +0300

    update js file

---
+++ 24-StickyNav/app.js
@@ -0,0 +1,14 @@
+const navEls = document.querySelector('#nav-els');
+let topOfNav = navEls.offsetTop;
+
+function fixedNav(){
+    if (window.scrollY >= topOfNav){
+        document.body.style.paddingTop =navEls.offsetHeiht + 'px';
+        document.body.classList.add('fixed-nav');
+    }else{
+        document.body.classList.remove('fixed-nav');
+        document.body.style.paddingTop = 0;
+    }
+}
+
+window.addEventListener('scroll', fixedNav);
